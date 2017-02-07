def0d7f1208f0be18081d4b6ac3b7e72b30e7334
Coke <cokejcm@gmail.com>
2017-02-07 07:47:31 +0100

    Unit tests

---
+++ src/test/java/Controller/RestResourceTest.java
@@ -0,0 +1,47 @@
+package Controller;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.glassfish.jersey.test.DeploymentContext;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.ServletDeploymentContext;
+import org.glassfish.jersey.test.grizzly.GrizzlyWebTestContainerFactory;
+import org.glassfish.jersey.test.spi.TestContainerFactory;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.demo.app.configuration.JerseyConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Application.class)
+@SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RestResourceTest extends JerseyTest {
+
+	// @Autowired
+	// private ArbitraryService service;
+
+	// Mockito.when(service.getEcho(input)).thenReturn(output);
+
+	@Override
+	protected TestContainerFactory getTestContainerFactory() {
+		return new GrizzlyWebTestContainerFactory();
+	}
+
+	@Override
+	protected DeploymentContext configureDeployment() {
+		return ServletDeploymentContext.forServlet(new ServletContainer(new JerseyConfig())).build();
+	}
+
+	@Test
+	public void testHello() {
+		final String hello = target("messages").request().get(String.class);
+		System.out.println(hello);
+	}
+}
