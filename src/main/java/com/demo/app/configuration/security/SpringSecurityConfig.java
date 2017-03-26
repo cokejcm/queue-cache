727f507f527946a2febbe9a1012caed1c0b051a6
cokejcm <cokejcm@gmail.com>
2017-03-27 01:11:43 +0200

    Spring Security

--- src/main/java/com/demo/app/configuration/security/SpringSecurityConfig.java
+++ src/main/java/com/demo/app/configuration/security/SpringSecurityConfig.java
@@ -1,47 +1,57 @@
 package com.demo.app.configuration.security;
 
-//@Configuration
-//@EnableWebSecurity
-//@Order(2)
-public class SpringSecurityConfig {// extends WebSecurityConfigurerAdapter {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-	// private final UserService userService;
-	// private final TokenAuthenticationService tokenAuthenticationService;
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	/*
-	 * public SpringSecurityConfig() { super(true); this.userService = new
-	 * UserService(); tokenAuthenticationService = new
-	 * TokenAuthenticationService("tooManySecrets", userService); }
-	 * 
-	 * @Override protected void configure(HttpSecurity http) throws Exception {
-	 * http .exceptionHandling().and()
-	 * .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.
-	 * STATELESS).and() // .headers().cacheControl().and().and()
-	 * .authorizeRequests() .antMatchers("/login").permitAll() // .antMatchers(
-	 * // HttpMethod.GET, // "/", // "/*.html", // "/favicon.ico",
-	 * //.antMatchers("/**").authenticated(); // All other request need to be
-	 * authenticated // .anyRequest().authenticated(); // .and();
-	 * 
-	 * // http // Custom Token based authentication based on the header //
-	 * previously given to the client // .addFilterBefore(new //
-	 * StatelessAuthenticationFilter(tokenAuthenticationService), //
-	 * UsernamePasswordAuthenticationFilter.class); /*}
-	 * 
-	 * @Override protected void configure(AuthenticationManagerBuilder auth)
-	 * throws Exception { auth.userDetailsService(userDetailsService())
-	 * .passwordEncoder(new BCryptPasswordEncoder()); }
-	 * 
-	 * @Bean
-	 * 
-	 * @Override public AuthenticationManager authenticationManagerBean() throws
-	 * Exception { return super.authenticationManagerBean(); }
-	 * 
-	 * @Bean
-	 * 
-	 * @Override public UserService userDetailsService() { return userService; }
-	 * 
-	 * @Bean public TokenAuthenticationService tokenAuthenticationService() {
-	 * return tokenAuthenticationService; }
-	 */
+	private final UserService userService;
+	private final TokenAuthenticationService tokenAuthenticationService;
+
+	public SpringSecurityConfig() {
+		super(true);
+		this.userService = new UserService();
+		tokenAuthenticationService = new TokenAuthenticationService("tooManySecrets", userService);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+
+				.anonymous().and().authorizeRequests().antMatchers("/login").permitAll()
+				.anyRequest().authenticated();
+
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		System.out.println("configure auth");
+		auth.userDetailsService(userDetailsService()).passwordEncoder(new BCryptPasswordEncoder());
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
+	@Override
+	public UserService userDetailsService() {
+		return userService;
+	}
+
+	@Bean
+	public TokenAuthenticationService tokenAuthenticationService() {
+		return tokenAuthenticationService;
+	}
 
 }
