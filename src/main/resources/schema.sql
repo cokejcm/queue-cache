655a7205c148757f10197c2dca2458afa9be990c
cokejcm <cokejcm@gmail.com>
2017-04-04 23:45:43 +0200

    Spring configuration

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,10 +7,6 @@ CREATE TABLE IF NOT EXISTS security.users (
   ACCOUNT_NON_EXPIRED boolean,
   ACCOUNT_NON_LOCKED  boolean, 
   PRIMARY KEY (username));
-
-INSERT INTO security.users (USERNAME, PASSWORD, ENABLED, ACCOUNT_NON_EXPIRED, ACCOUNT_NON_LOCKED) VALUES
-	('krishna', '$2a$10$bwG9f.T16qMfnegO.1CX0.hflrX3mrnK0n9saWipFppbyXu8O26Lm', true, true, true),
-	('sudama', '$2a$10$bwG9f.T16qMfnegO.1CX0.hflrX3mrnK0n9saWipFppbyXu8O26Lm', true, true, true); 
 	
 CREATE TABLE IF NOT EXISTS security.user_authorities (
   ID TEXT NOT NULL,
@@ -18,8 +14,3 @@ CREATE TABLE IF NOT EXISTS security.user_authorities (
   AUTHORITY TEXT NOT NULL,
   UNIQUE (USERNAME, AUTHORITY),
   CONSTRAINT fk_authorities_users FOREIGN KEY (USERNAME) REFERENCES security.users (USERNAME));
-
-INSERT INTO security.user_authorities (ID, USERNAME, AUTHORITY) VALUES
-	('1', 'krishna', 'ROLE_ADMIN'),
-	('2', 'sudama', 'ROLE_USER'),
-	('2', 'krishna', 'ROLE_USER');
