1e372a758f45fb9bb9da3c62488f88a999b0a02f
jcarreno <jcarreno@atsistemas.com>
2017-05-03 16:53:28 +0200

    Spring AOP

--- src/main/java/com/demo/app/configuration/aop/SampleAspect.java
+++ src/main/java/com/demo/app/configuration/aop/SampleAspect.java
@@ -5,15 +5,15 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import com.demo.app.domain.Entity;
+
 @Aspect
 @Component
 public class SampleAspect {
 
-	@AfterReturning(
-	pointcut = "execution(* com.demo.app.controller.MessageController.messageById(..))",
-	returning= "result")
+	@AfterReturning(pointcut = "execution(* com.demo.app.controller.*.*(..))", returning= "result")
 	public void logExecution (JoinPoint joinPoint, Object result){
-		System.out.println("Reading id: " + (String)result);
+		System.out.println("Reading id: " + ((Entity)result).getId());
 	}
 
 }
