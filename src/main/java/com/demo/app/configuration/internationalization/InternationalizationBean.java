dde1363358863f80b7d015eda6ffa5d7b2e4b407
cokejcm <cokejcm@gmail.com>
2017-05-06 22:31:45 +0200

    i18n

--- src/main/java/com/demo/app/configuration/internationalization/InternationalizationBean.java
+++ src/main/java/com/demo/app/configuration/internationalization/InternationalizationBean.java
@@ -2,13 +2,7 @@ package com.demo.app.configuration.internationalization;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-
-import com.demo.app.util.Constants;
 
 @Configuration
 public class InternationalizationBean extends WebMvcConfigurerAdapter {
@@ -19,24 +13,5 @@ public class InternationalizationBean extends WebMvcConfigurerAdapter {
 		messageSource.setBasename("classpath:messages");
 		messageSource.setDefaultEncoding("UTF-8");
 		return messageSource;
-	}
-
-	@Bean
-	public LocaleResolver localeResolver() {
-		CookieLocaleResolver localeResolver = new CookieLocaleResolver();
-		localeResolver.setCookieName(Constants.COOKIE_LANGUAGE);
-		return localeResolver;
-	}
-
-	@Bean
-	public LocaleChangeInterceptor localeInterceptor() {
-		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-		interceptor.setParamName("lang");
-		return interceptor;
-	}
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(localeInterceptor());
 	}
 }
