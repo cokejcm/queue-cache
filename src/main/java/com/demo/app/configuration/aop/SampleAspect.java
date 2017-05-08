62746775d1de53378f88ce2d1c92ce03842705a6
jcarreno <jcarreno@atsistemas.com>
2017-05-08 17:47:36 +0200

    Logger

--- src/main/java/com/demo/app/configuration/aop/SampleAspect.java
+++ src/main/java/com/demo/app/configuration/aop/SampleAspect.java
@@ -1,5 +1,7 @@
 package com.demo.app.configuration.aop;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,10 +13,21 @@ import com.demo.app.domain.Entity;
 @Component
 public class SampleAspect {
 
+	private static final Logger LOGGER = LogManager.getLogger(SampleAspect.class);
+
 	@AfterReturning(pointcut = "execution(* com.demo.app.controller.*.*(..))", returning= "result")
 	public void logExecution (JoinPoint joinPoint, Object result){
 		if (result instanceof Entity){
-			System.out.println("Reading id: " + ((Entity)result).getId());
+			LOGGER.info("Reading id: " + ((Entity)result).getId());
+			LOGGER.debug("This is a debug message");
+
+			LOGGER.info("This is an info message");
+
+			LOGGER.warn("This is a warn message");
+
+			LOGGER.error("This is an error message");
+
+			LOGGER.fatal("This is a fatal message");
 		}
 	}
 }
