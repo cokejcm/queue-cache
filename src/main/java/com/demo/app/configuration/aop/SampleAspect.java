c58b8bd63b4fc648f45d757fc95266fca5392287
jcarreno <jcarreno@atsistemas.com>
2017-05-09 12:15:20 +0200

    Logback

--- src/main/java/com/demo/app/configuration/aop/SampleAspect.java
+++ src/main/java/com/demo/app/configuration/aop/SampleAspect.java
@@ -1,10 +1,10 @@
 package com.demo.app.configuration.aop;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.demo.app.domain.Entity;
@@ -13,21 +13,17 @@ import com.demo.app.domain.Entity;
 @Component
 public class SampleAspect {
 
-	private static final Logger LOGGER = LogManager.getLogger(SampleAspect.class);
+	private  final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@AfterReturning(pointcut = "execution(* com.demo.app.controller.*.*(..))", returning= "result")
 	public void logExecution (JoinPoint joinPoint, Object result){
+		logger.debug("This is a debug message");
+		logger.info("This is an info message");
+		logger.warn("This is a warn message");
+		logger.error("This is an error message");
 		if (result instanceof Entity){
-			LOGGER.info("Reading id: " + ((Entity)result).getId());
-			LOGGER.debug("This is a debug message");
-
-			LOGGER.info("This is an info message");
-
-			LOGGER.warn("This is a warn message");
-
-			LOGGER.error("This is an error message");
-
-			LOGGER.fatal("This is a fatal message");
+			logger.info("Reading id: " + ((Entity)result).getId());
 		}
+
 	}
 }
