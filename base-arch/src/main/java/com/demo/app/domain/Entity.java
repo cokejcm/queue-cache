2199d0cadd31bf73990b94d6db9c289efe96083a
jcarreno <jcarreno@atsistemas.com>
2017-05-17 17:19:41 +0200

    JPA Dao

--- base-arch/src/main/java/com/demo/app/domain/Entity.java
+++ base-arch/src/main/java/com/demo/app/domain/Entity.java
@@ -2,9 +2,8 @@ package com.demo.app.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-
-import org.springframework.data.annotation.Id;
 
 @MappedSuperclass
 public abstract class Entity implements Serializable {
@@ -12,7 +11,7 @@ public abstract class Entity implements Serializable {
 	private static final long serialVersionUID = 5908519522358747038L;
 
 	@Id
-	private String id;
+	String id;
 
 	public String getId() {
 		return this.id;
