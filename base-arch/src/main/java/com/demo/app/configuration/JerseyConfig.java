13791bc5d2decfa0d07f5429b96dcfa06e643131
cokejcm <cokejcm@gmail.com>
2017-05-22 00:39:40 +0200

    Generic Controller

--- base-arch/src/main/java/com/demo/app/configuration/JerseyConfig.java
+++ base-arch/src/main/java/com/demo/app/configuration/JerseyConfig.java
@@ -1,12 +1,9 @@
 package com.demo.app.configuration;
 
-import static org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType.HAL;
-
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
-import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 /**
  * Provides the location of the packages with the Rest resources and @Providers
@@ -14,7 +11,6 @@ import org.springframework.hateoas.config.EnableHypermediaSupport;
  * @author coke
  *
  */
-@EnableHypermediaSupport(type = HAL)
 public class JerseyConfig extends ResourceConfig {
 
 	public JerseyConfig() {
