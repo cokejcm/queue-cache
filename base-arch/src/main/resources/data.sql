02146f0dc2a25a2b2d58293a3e7fbad942cbfc5d
jcarreno <jcarreno@atsistemas.com>
2017-08-02 10:39:42 +0200

    Added user1

--- base-arch/src/main/resources/data.sql
+++ base-arch/src/main/resources/data.sql
@@ -1,10 +1,12 @@
 INSERT INTO security.users (USERNAME, PASSWORD, ENABLED, ACCOUNT_NON_EXPIRED, ACCOUNT_NON_LOCKED) VALUES
 	('krishna', '$2a$10$bwG9f.T16qMfnegO.1CX0.hflrX3mrnK0n9saWipFppbyXu8O26Lm', true, true, true),
 	('sudama', '$2a$10$bwG9f.T16qMfnegO.1CX0.hflrX3mrnK0n9saWipFppbyXu8O26Lm', true, true, true),
-	('superuser', '$2a$10$bwG9f.T16qMfnegO.1CX0.hflrX3mrnK0n9saWipFppbyXu8O26Lm', true, true, true); 
+	('superuser', '$2a$10$bwG9f.T16qMfnegO.1CX0.hflrX3mrnK0n9saWipFppbyXu8O26Lm', true, true, true),
+	('user1', '$2a$10$bwG9f.T16qMfnegO.1CX0.hflrX3mrnK0n9saWipFppbyXu8O26Lm', true, true, true);
 
 INSERT INTO security.user_authorities (ID, USERNAME, AUTHORITY) VALUES
 	('1', 'krishna', 'ADMIN_ROLE'),
 	('2', 'sudama', 'BASIC_ROLE'),
-	('3', 'krishna', 'BASIC_ROLE');
-	('4', 'superuser', 'APP_ROLE');	
+	('3', 'krishna', 'BASIC_ROLE'),
+	('4', 'superuser', 'APP_ROLE'),
+	('5', 'user1', 'BASIC_ROLE');
